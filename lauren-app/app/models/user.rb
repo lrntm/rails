610a7248a8ca90d760a57b364a5d7143a6bfc30c
lrntm <lmt2172@barnard.edu>
2015-09-21 20:02:03 -0400

    used scaffold to generate User

--- lauren-app/app/models/user.rb
+++ lauren-app/app/models/user.rb
@@ -1,8 +1,6 @@
 class User
-	include Mongoid::Document
-
-	field :first_name,	:type => String
-	field :last_name,	:type => String
-	field :age, 		:type => Double
-
+  include Mongoid::Document
+  field :first_name, type: String
+  field :last_name, type: String
+  field :email, type: String
 end
