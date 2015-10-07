3ff28c8e9d7966928d4e5f0fe2b0bc596be25b8e
lrntm <lmt2172@barnard.edu>
2015-10-06 22:16:07 -0400

    Create a Model to represent items in the CSV

--- lauren-app/app/models/market.rb
+++ lauren-app/app/models/market.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-class market
+class Market
   include Mongoid::Document
 
   field :borough, type: String
@@ -11,12 +11,5 @@ class market
   field :health_bucks, type: Boolean
   field :ebt, type: Boolean
   field :stellar, type: Boolean
-
-  def self.populate_data_from_csv
-    file_path = Rails.root.join 'app', 'assets', 'csvs', 'names.csv'
-    CSV.foreach(file_path, :headers => true, :col_sep => ',') do |row|
-      Name.create(:birth_year => row['BRTH_YR'], :gender => row['GNDR'], :ethnicity => row['ETHCTY'], :name => row['NM'], :count => row['CNT'], :rank => row['RNK'])
-    end
-  end
 
 end
